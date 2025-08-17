6fcb97e3c6d450038dfd6dcab78bba3a91d017fe
elvis jonathan <ighoelvis8@gmail.com>
2025-08-17 21:39:19 +0100

    Added flask_cor

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 from app.config import Config
 from app.extensions import db, jwt, migrate
 from app.scrapper import run_post_scrapper
-
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    CORS(app)
     db.init_app(app)
     jwt.init_app(app)
     migrate.init_app(app, db)
